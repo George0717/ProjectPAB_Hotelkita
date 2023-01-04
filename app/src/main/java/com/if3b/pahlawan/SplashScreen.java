c6ea9d44ac6030d3c1e58e81e7c41c363bac294b
George0717 <raden7george@gmail.com>
2023-01-04 15:15:00 +0700

    add SplashScreen

---
+++ app/src/main/java/com/if3b/pahlawan/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.if3b.pahlawan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Handler hdl = new Handler();
+
+        hdl.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
+
